6f3f9486b846490ef8b314f7404cd7d0ac327c35
MAKC <maxim.s.lazarev@gmail.com>
2020-01-31 18:04:02 +0300

    Add model records

--- models/customer/CustomerRecord.php
+++ models/customer/CustomerRecord.php
@@ -1,12 +1,26 @@
 <?php
 
-namespace app\models;
+namespace app\models\customer;
 
 use yii\db\ActiveRecord;
 
 class CustomerRecord extends ActiveRecord
-
-public static function tableName()
 {
-    return 'customer';
+    public static function tableName()
+    {
+        return 'customer';
+    }
+
+    public function rules()
+    {
+        return [
+            ['id', ' number'],
+            ['name', 'required'],
+            ['name', 'string', 'max' => 256],
+            ['birth_date', 'date', 'format' => 'Y-m-d'],
+            ['notes', 'safе']
+        ];
+    }
 }
+
+
--- models/customer/PhoneRecord.php
+++ models/customer/PhoneRecord.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace app\models\customer;
 
+use yii\db\ActiveRecord;
 
-class PhoneRecord
+class PhoneRecord extends ActiveRecord
 {
+    public static function tableName()
+    {
+        return 'phone';
+    }
+
+    public function rules()
+    {
+        return [
+            ['customer_id', ' number'],
+            ['number', 'string'],
+            [['customer_id', 'number'], 'required']
+        ];
+    }
 
 }
