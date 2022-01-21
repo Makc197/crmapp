16edd7737fa8e2cda497fa46e1a713d69331a184
MAKC <maxim.s.lazarev@gmail.com>
2022-01-21 20:06:14 +0300

    Add some acceptance tests

---
+++ tests/acceptance/AdminAccessRightsCept.php
@@ -0,0 +1,34 @@
+<?php
+namespace Step\Acceptance;
+
+$I = new CRMUsersManagementSteps($scenario);
+$I->wantTo('Check Manager-level access rights');
+
+$I->amOnPage('/customers/query');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/customers/index');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/customers/add');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/create');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/services/view');
+$I->dontSee('Forbidden');
+
+
+$I->amOnPage('/users/create');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/users/index');
+$I->dontSee('Forbidden');
+
+$I->amOnPage('/users/view');
+$I->dontSee('Forbidden');
